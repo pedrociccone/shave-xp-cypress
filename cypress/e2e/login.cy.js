9ef01a24a3b1de00cbba58e81594908877f9959d
“Pedro <pedrocicc14@gmail.com>
2023-03-23 21:28:50 -0300

    Refactored the treatment of testing data.

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,27 +2,25 @@
 import loginPage from '../support/pages/login'
 import shaversPage from '../support/pages/shavers'
 
+import data from '../fixtures/users-login.json'
+
 describe('Login', () => {
 
     context('Quando submeto o formulário', () => {
+        it.only('Deve logar com sucesso', () => {
+            // Dado que eu tenho um NOVO usuário cadastrado
+            const user = data.success
+            cy.createUser(user)
 
-        it('Deve logar com sucesso', () => {
-            const user = {
-                name: 'Pedro',
-                email: 'pedrocicc14@gmail.com',
-                password: 'qax123'
-            }
-
+            // Quando submeto o form de login com esse usuário
             loginPage.submit(user.email, user.password)
+
+            // Então devo ser logado com sucesso
             shaversPage.header.userShouldBeLoggedIn(user.name)
         })
 
         it('Não deve logar com senha incorreta', () => {
-            const user = {
-                name: 'Pedro',
-                email: 'pedrocicc14@gmail.com',
-                password: 'qax456'
-            }
+            const user = data.invpass
 
             loginPage.submit(user.email, user.password)
 
@@ -31,11 +29,7 @@ describe('Login', () => {
         })
 
         it('Não deve logar com email não cadastrado', () => {
-            const user = {
-                name: 'Pedro',
-                email: 'pedrocicc@gmail.com',
-                password: 'qax456'
-            }
+            const user = data.email404
 
             loginPage.submit(user.email, user.password)
 
@@ -50,45 +44,22 @@ describe('Login', () => {
     })
 
     context('Senha muito curta', () => {
-
-        const passwords = [
-            '1',
-            '12',
-            '123',
-            '1234',
-            '12345'
-        ]
-
         // p = unit from the test vector
-        passwords.forEach((p) => {
+        data.shortpass.forEach((p) => {
             it(`Não deve logar com a senha: ${p}`, () => {
                 loginPage.submit('pedrocicc14@gmail.com', p)
                 loginPage.alertShouldBe('Pelo menos 6 caracteres')
             })
         })
-
     })
 
     context('Email no formato incorreto', () => {
-
-        const emails = [
-            'pedro&gmail.com',
-            'pedro.com.br',
-            '@gmail.com',
-            '@',
-            'pedro@',
-            '12313432',
-            '#@$$!#@',
-            'xpars3213'
-        ]
-
-        emails.forEach((e) => {
+        data.invemails.forEach((e) => {
             it(`Não deve logar com o e-mail: ${e}`, () => {
                 loginPage.submit(e, 'qax123')
                 loginPage.alertShouldBe('Informe um email válido')
             })
         })
-
     })
 
 })
