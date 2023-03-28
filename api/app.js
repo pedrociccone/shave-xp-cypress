5ee9cf1e02855a369f5f1bce893d05bed5d4fbea
“Pedro <pedrocicc14@gmail.com>
2023-03-28 12:44:15 -0300

    Corrected some details on the comments.

--- api/app.js
+++ api/app.js
@@ -57,13 +57,13 @@ app.post('/user', validator.body(userSchema), async function (req, res) {
 
 app.use((err, req, res, next) => {
     if (err && err.error && err.error.isJoi) {
-        // we had a joi error, let's return a custom 400 json response
+        // We had a joi error, let's return a custom 400 json response
         res.status(400).json({
-            type: err.type, // will be "query" here, but could be "headers", "body", or "params"
+            type: err.type, // Will be "query" here, but could be "headers", "body", or "params"
             message: err.error.toString()
         });
     } else {
-        // pass on to another error handler
+        // Pass on to another error handler
         next(err);
     }
 });
