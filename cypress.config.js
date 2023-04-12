dc26d98cbc807504a68949451596376c752ad2f1
“Pedro <pedrocicc14@gmail.com>
2023-04-12 19:42:21 -0300

    Adding environment config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const { defineConfig } = require("cypress");
 const { removeUser } = require('./cypress/support/tasks/database')
 
@@ -9,8 +11,12 @@ module.exports = defineConfig({
         removeUser
       })
     },
+    env: {
+      apiUrl: process.env.API_URL,
+      apiHelper: process.env.API_HELPER
+    },
     viewportWidth: 1920,
     viewportHeight: 1080,
-    baseUrl: 'http://localhost:3000'
+    baseUrl: process.env.BASE_URL
   },
 });
--- cypress/support/commands/api.js
+++ cypress/support/commands/api.js
@@ -1,7 +1,7 @@
 Cypress.Commands.add('createUser', (user) => {
     cy.request({
         method: 'POST',
-        url: 'http://localhost:5000/user',
+        url: Cypress.env('apiHelper') + '/user',
         body: user
     }).then(function (response) {
         expect(response.status).to.eq(201)
@@ -11,7 +11,7 @@ Cypress.Commands.add('createUser', (user) => {
 Cypress.Commands.add('deleteUser', (user) => {
     cy.request({
         method: 'DELETE',
-        url: 'http://localhost:5000/user/' + user.email,
+        url: Cypress.env('apiHelper') + '/user' + user.email,
     }).then(function (response) {
         expect(response.status).to.eq(204)
     })
@@ -20,7 +20,7 @@ Cypress.Commands.add('deleteUser', (user) => {
 Cypress.Commands.add('recoverPass', (email) => {
     cy.request({
         method: 'POST',
-        url: 'http://localhost:3333/password/forgot',
+        url: Cypress.env('apiUrl') + '/password/forgot',
         body: { email: email }
     }).then(result => {
         expect(result.status).to.eql(204)
@@ -30,7 +30,7 @@ Cypress.Commands.add('recoverPass', (email) => {
 Cypress.Commands.add('getToken', (email) => {
     cy.request({
         method: 'GET',
-        url: 'http://localhost:5000/token/' + email
+        url: Cypress.env('apiHelper') + '/token/' + email
     }).then(result => {
         expect(result.status).to.eql(200)
         cy.log(result.body.token)
@@ -43,7 +43,7 @@ Cypress.Commands.add('getToken', (email) => {
 Cypress.Commands.add('apiLogin', (user) => {
     cy.request({
         method: 'POST',
-        url: 'http://localhost:3333/sessions',
+        url: Cypress.env('apiUrl') + '/sessions',
         body: {
             email: user.email,
             password: user.password
